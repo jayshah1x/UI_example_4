fa417b071410bdb5f083edb8879d4d521b96a543
Jay shah <jayshahx01@gmail.com>
2020-04-22 02:31:40 +0530

    Uploaded 22/04/20

--- app/src/main/java/com/bvm/ui_example_4/MainActivity.java
+++ app/src/main/java/com/bvm/ui_example_4/MainActivity.java
@@ -37,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
         Username = findViewById(R.id.username);
         Password = findViewById(R.id.password);
         Login = findViewById(R.id.login);
@@ -46,9 +49,16 @@ public class MainActivity extends AppCompatActivity {
         Register_btn = findViewById(R.id.register);
         remember = findViewById(R.id.remember);
 
-        checkElement();
+
         sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
 
+        Register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Second.class);
+                startActivity(intent);
+            }
+        });
 
         Login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -110,8 +120,7 @@ public class MainActivity extends AppCompatActivity {
                         else{
                             sharedPreferences.edit().clear().apply();
                         }
-                        Intent intent = new Intent(MainActivity.this, Second.class);
-                        startActivity(intent);
+                        Toast.makeText(MainActivity.this,"Your are logged in", Toast.LENGTH_SHORT).show();
                 }
                 }
             }
@@ -150,49 +159,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*private  Boolean validatePassword(){
-        String password = Password.getText().toString();
-        if(password.isEmpty()){
-            Username.setError("Field cannot be empty");
-            return false;
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                             |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
-        else if(password.matches("$@_-&%!^")){
-            Username.setError("Password cannot contain special character $@_-&^%!");
-            return false;
-        }
-        else{
-            Username.setError(null);
-            return true;
-        }
-    }*/
-
-    private void checkElement() {
-
-        Username.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus)
-                {
-                    user_var = Username.getText().toString();
-                    if(user_var.equals("                      Username"))
-                    {
-                        Username.setText("");
-                    }
-                }
-            }
-        });
-        Password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus)
-                {
-                    pass_var = Password.getText().toString();
-                    if(pass_var.equals("                      Password"))
-                    {
-                        Password.setText("");
-                    }
-                }
-            }
-        });
     }
 }
